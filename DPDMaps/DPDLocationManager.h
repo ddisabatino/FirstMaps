7cd7d62dd298a48c4a3e36d35fe08d7b5124abd5
ddisabatino <ddisabatino92@gmail.com>
2013-04-29 03:51:36 -0400

    first commit

---
+++ DPDMaps/DPDLocationManager.h
@@ -0,0 +1,13 @@
+//
+//  DPDLocationManager.h
+//  DPDMaps
+//
+//  Created by Devin DiSabatino on 4/29/13.
+//  Copyright (c) 2013 Disabs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DPDLocationManager : NSObject
+
+@end
---
+++ DPDMaps/DPDLocationManager.m
@@ -0,0 +1,13 @@
+//
+//  DPDLocationManager.m
+//  DPDMaps
+//
+//  Created by Devin DiSabatino on 4/29/13.
+//  Copyright (c) 2013 Disabs. All rights reserved.
+//
+
+#import "DPDLocationManager.h"
+
+@implementation DPDLocationManager
+
+@end
